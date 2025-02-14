0b3ac92d0fe891e637e5ace2afcda6ceb0603f70
micpiwo38 <michel.michael38@gmail.com>
2025-02-14 12:46:25 +0100

    Ajout de la supression d'un compte utilisateur

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 
 
 const Profile = ({user, is_login}) =>{
   //States
-
+  const [current_user, setCurrentUser] = useState(user);
   //Actions
+  const delete_profile = (id) => {
+    axios.delete(`http://localhost:3000/users/${id}`)
+      .then(response => {
+        alert("Votre compte a été supprimer !");
+        window.location.href = '/inscription';
+      })
+      .catch(error => console.error("Erreur lors de la supression de votre compte !" + error));
+  }
 
   //Renderer
   return(
     <div>
       {is_login ?(
         <div className='alert alert-success'>
-            Bienvenue : {user.email}
+            <div>Bienvenue : {user.email}</div>
+            <button type='button' className='btn btn-danger mt-3' onClick={() => delete_profile(user.id)}>Supprimer votre compte</button>
         </div> 
       ):(
         <div className='alert alert-danger'>
