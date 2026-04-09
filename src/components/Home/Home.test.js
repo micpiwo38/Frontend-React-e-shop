741b9f57a96f85d0ff96afe9c28ac561daeed8c6
qiwebDev <qiweb.formation@gmail.com>
2026-04-09 12:33:41 +0200

    Config de CI - CD Read et write permissions

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -9,5 +9,5 @@ test("Afficher le titre de la page", () => {
   //Creer un faux navigateur + monter et afficher le composant + la fonction screen qui parcours le DOM et cherche des elements
   render(<Home name="Michael" />);
   //Reponse du test attendue
-  expect(screen.getByText("Hello Michael")).toBeInTheDocument();
+  expect(screen.getByText("Hello Michael")).toBeInTheDocument(); //Atention au espace entre les elements => le texte est sensible a la case
 });
